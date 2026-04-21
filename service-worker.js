f4506521431788c27931e64eb72edb5bb0c0420d
ccstorebp18-cyber <ccstore.bp18@gmail.com>
2026-04-21 18:24:46 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,27 @@
-const CACHE_NAME = "manhwa-app-v2"; // ganti versi biar refresh cache
+const CACHE_NAME = "manhwa-app-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/logo.png",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./logo.png",
+  "./manifest.json"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
